2cc762eab60a0c40b40c705e5f619a0320ec98fc
wellyngson <wellyngson.guedes@consulting-for.edenred.com>
2024-01-05 19:38:41 -0300

    feat: add credentials to github

--- wizard/build.gradle.kts
+++ wizard/build.gradle.kts
@@ -1,6 +1,11 @@
+import org.gradle.api.publish.maven.MavenPublication
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
 
 android {
@@ -50,4 +55,33 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+val githubProperties = Properties()
+githubProperties.load(FileInputStream(rootProject.file("github.properties")))
+
+val getVersionName = { "1.0.0" } // Replace with version Name
+val getArtifactId = { "wizardds" } // Replace with library name ID
+
+publishing {
+    publications {
+        create<MavenPublication>("bar") {
+            groupId = "com.touchetime.wizard" // Replace with group ID
+            artifactId = getArtifactId()
+            version = getVersionName()
+            artifact("$buildDir/outputs/aar/${getArtifactId()}-release.aar")
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/GITHUB_USERID/REPOSITORY")
+
+            credentials {
+                username = (githubProperties["gpr.usr"] ?: System.getenv("GPR_USER")).toString()
+                password = (githubProperties["gpr.key"] ?: System.getenv("GPR_API_KEY")).toString()
+            }
+        }
+    }
 }
